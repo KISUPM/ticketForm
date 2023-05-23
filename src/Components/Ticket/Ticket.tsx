e7bee12c18dd819c617ece3a6290f523a4f38021
KISUPM <panzer_thai@hotmail.com>
2023-05-23 16:43:00 +0700

    feat:ticketForm

---
+++ src/Components/Ticket/Ticket.tsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import { useForm, Controller } from 'react-hook-form'
+import { FormControl, FormLabel, Button, Input, HStack, Box, Select, Textarea } from "@chakra-ui/react"
+
+export default function Ticket() {
+    const { handleSubmit, control, reset, formState: { errors } } = useForm();
+
+    const onSubmitData = (data: any) => {
+        console.clear();
+        console.log(data);
+    }
+
+    const clearForm = () => {
+        const form = document.getElementById("form") as HTMLFormElement;
+        form.reset();
+        reset();
+    }
+    return (
+        <form id="form" onSubmit={handleSubmit(onSubmitData)}>
+            <Box p="20px">
+                <Controller
+                    name="senderName"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => (
+                        <FormControl isRequired isInvalid={Boolean(errors["senderName"]!)}>
+                            <FormLabel>Name</FormLabel>
+                            <Input type="text" {...field} />
+                            {/* <FormErrorMessage>
+                            {errors["senderName"]!&&<p>{String(errors["senderName"]?.message)}</p>}
+                        </FormErrorMessage> */}
+                        </FormControl>
+                    )}
+                />
+
+                <Controller
+                    name="senderEmail"
+                    control={control}
+                    defaultValue={""}
+                    render={({ field }) => (
+                        <FormControl isRequired isInvalid={Boolean(errors["senderEmail"])}>
+                            <FormLabel>Email</FormLabel>
+                            <Input type="email" {...field} />
+                        </FormControl>
+                    )}
+                />
+
+                <Controller
+                    name="ticketSubject"
+                    control={control}
+                    defaultValue={""}
+                    render={({ field }) => (
+                        <FormControl isRequired isInvalid={Boolean(errors["ticketSubject"])}>
+                            <FormLabel>Subject</FormLabel>
+                            <Input type="text" {...field} />
+                        </FormControl>
+                    )}
+                />
+
+                <Controller
+                    name="ticketDescription"
+                    control={control}
+                    defaultValue={""}
+                    render={({ field }) => (
+                        <FormControl isRequired isInvalid={Boolean(errors["ticketDescription"])}>
+                            <FormLabel>Description</FormLabel>
+                            <Textarea {...field} />
+                        </FormControl>
+                    )}
+                />
+
+                <Controller
+                    name="ticketPriority"
+                    control={control}
+                    defaultValue={""}
+                    render={({ field }) => (
+                        <FormControl isRequired isInvalid={Boolean(errors["ticketPriority"])}>
+                            <FormLabel>Priority</FormLabel>
+                            <Select {...field} placeholder="please select priority level">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                            </Select>
+                        </FormControl>
+                    )}
+                />
+
+                <Controller
+                    name="ticketImage"
+                    control={control}
+                    defaultValue={""}
+                    render={({ field }) => (
+                        <FormControl>
+                            <FormLabel>Attractment</FormLabel>
+                            <Input
+                                type="file"
+                                accept='image/png,image/jpg,image/jpeg'
+                                {...field}
+                            />
+                        </FormControl>
+                    )}
+                />
+                <Box>
+                    <HStack justifyContent={"space-around"} mt={"20px"}>
+                        <Button colorScheme='teal' type="submit">Submit</Button>
+                        <Button colorScheme='teal' onClick={clearForm}>Clear Form</Button>
+                    </HStack>
+                </Box>
+            </Box>
+        </form>
+
+    )
+}
